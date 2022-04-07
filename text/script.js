aadc33d15330453443493e58c9bb8af7f089ad92
ahsanabdullaa <73553412+ahsanabdullaa@users.noreply.github.com>
2022-04-07 20:16:23 +0500

    Delete text directory

--- text/script.js
+++
@@ -1,8 +0,0 @@
-//nav bar animation over scroll
-$(window).on("scroll",function(){
-    if($(window).scrollTop() >= 300){
-        $(".header").addClass("fixed");
-    }
-    else
-        $(".header").removeClass("fixed");
-})
