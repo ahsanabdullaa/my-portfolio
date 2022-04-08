f148c6ba1d4253fdc3cf09595ffb4ae6e79033c4
ahsanabdullaa <73553412+ahsanabdullaa@users.noreply.github.com>
2022-04-08 20:06:10 +0500

    Update script.js

--- text/script.js
+++ text/script.js
@@ -6,3 +6,13 @@ $(window).on("scroll",function(){
     else
         $(".header").removeClass("fixed");
 })
+
+//smooth scroll by clicking on navbar items
+var $root = $('html, body');
+$('a[href^="#"]').click(function () {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+
+    return false;
+});
